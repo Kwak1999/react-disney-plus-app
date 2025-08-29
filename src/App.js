9384ab3d109a9415f995be187db96909fcef6798
HuiBeom <gmlqja7913@daum.net>
2025-08-29 20:45:43 +0900

    비디오 배너 생성하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
 import Nav from "./components/Nav";
+import styled from "styled-components";
 
 
 function App() {
   return (
     <div className="App">
-        <Nav />
+        <Container>
+            <Nav />
+        </Container>
+
     </div>
   );
 }
 
 export default App;
+
+
+const Container = styled.main`
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow: hidden;
+    display: block;
+    top: 72px;
+    padding: 0 calc(3.5vw + 5px);
+    
+    &:after {
+        background: url("/images/home-background.png")
+        center center / cover no-repeat fixed;
+        content: "";
+        position: absolute;
+        inset: 0px;
+        opacity: 1;
+        z-index: -1;
+    }
+`
